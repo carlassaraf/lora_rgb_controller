882a45b891c0ef3f43734763647008f1bb606dd2
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2026-04-22 01:44:28 -0300

    feat: add sd card reading file from mqtt payload

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,24 @@
 #include <Arduino.h>
-#include "lora_sm.h"
 #include "network_sm.h"
+#include "sd_manager.h"
 
-#define NODE_ID 0x01
-#define CS_PIN 8
-#define RST_PIN 4
-#define DIO0_PIN 7
+#define SD_CS_PIN 3
+#define SD_CHUNK_SIZE 32
 
+static uint8_t s_frame_buf[SD_CHUNK_SIZE];
 
 void setup() {
-#ifdef DEBUG
+  
   Serial.begin(115200);
-  while(!Serial);
-#endif
-
-  lora_configuration_t lora_config = {
-    .node_id = NODE_ID,
-    .cs_pin = CS_PIN,
-    .rst_pin = RST_PIN,
-    .dio0_pin = DIO0_PIN,
-    .frequency = (uint32_t)915E6
-  };
-  lora_sm_set_configuration(&lora_config);
-
+  while (!Serial);
+  // Start SD manager
+  sd_manager_init(SD_CS_PIN);
+  // Network configuration
   network_configuration_t network_config = {0};
-  strcpy(network_config.id, "lora32u4");
-  strcpy(network_config.apn, "datos.personal.com");
+  strcpy(network_config.id,     "lora32u4");
+  strcpy(network_config.apn,    "datos.personal.com");
   strcpy(network_config.broker, "broker.hivemq.com");
-  strcpy(network_config.topic, "test/lora32u4");
+  strcpy(network_config.topic,  "test/lora32u4");
   if (!network_sm_set_configuration(&network_config)) {
 #ifdef DEBUG
     Serial.println(F("Failed to configure Network SM"));
@@ -38,49 +29,47 @@ void setup() {
 
 void loop() {
 
-
   // --- Network SM ---
 
 #ifdef DEBUG
-  network_sm_state_t network_sm_prev = network_sm_get_state();
+  network_sm_state_t net_prev = network_sm_get_state();
 #endif
-  network_sm_state_t network_sm_curr = network_sm_run();
+  network_sm_state_t net_curr = network_sm_run();
 #ifdef DEBUG
-  if (network_sm_prev != network_sm_curr) {
-    char state_str[64];
-    sprintf(state_str, "network_sm: %s -> %s",
-            network_sm_state_to_string(network_sm_prev),
-            network_sm_state_to_string(network_sm_curr));
-    Serial.println(state_str);
+  if (net_prev != net_curr) {
+    char buf[64];
+    sprintf(buf, "network_sm: %s -> %s",
+            network_sm_state_to_string(net_prev),
+            network_sm_state_to_string(net_curr));
+    Serial.println(buf);
   }
 #endif
-
+  // Check if there's any message available from MQTT
   if (network_sm_mqtt_message_available()) {
-    char topic[64];
-    char payload[64];
-
+    char topic[32];
+    char payload[32];
     network_sm_mqtt_get_message(topic, payload);
-
-    Serial.print("TOPIC: ");
-    Serial.println(topic);
-
-    Serial.print("PAYLOAD: ");
-    Serial.println(payload);
+#ifdef DEBUG
+    Serial.print(F("MQTT rx: ")); Serial.println(payload);
+#endif
+    // Try to find file in SD
+  if (!sd_manager_request_file(payload, s_frame_buf, SD_CHUNK_SIZE)) {
+#ifdef DEBUG
+      Serial.println(F("SD busy"));
+#endif
+    }
   }
 
-  // --- LoRa SM ---
+  // --- SD Manager ---
 
-#ifdef DEBUG
-  lora_sm_state_t lora_sm_prev = lora_sm_get_state();
-#endif
-  lora_sm_state_t lora_sm_curr = lora_sm_run();
-#ifdef DEBUG
-  if (lora_sm_prev != lora_sm_curr) {
-    char state_str[64];
-    sprintf(state_str, "lora_sm: %s -> %s",
-            lora_sm_state_to_string(lora_sm_prev),
-            lora_sm_state_to_string(lora_sm_curr));
-    Serial.println(state_str);
+  // Run SD state machine
+  sd_manager_run();
+  // Once requested, read data from SD
+  if (sd_manager_data_ready()) {
+    uint16_t n = sd_manager_get_bytes_read();
+    for (uint16_t i = 0; i < n; i++) {
+      Serial.write(s_frame_buf[i]);
+    }
+    sd_manager_consume();
   }
-#endif
 }
